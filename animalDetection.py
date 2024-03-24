9ae06c494d0faa4e9d9e7fe757a31b02a5bc1a0c
Matthew En <86386184+Mcen25@users.noreply.github.com>
2024-03-24 15:08:42 -0400

    Removed mp4

--- animalDetection.py
+++ animalDetection.py
@@ -46,6 +46,15 @@ class ObjectDetection:
         results = self.model(im0)
         return results
 
+    def display_fps(self, im0):
+        self.end_time = time()
+        fps = 1 / np.round(self.end_time - self.start_time, 2)
+        text = f'FPS: {int(fps)}'
+        text_size = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, 1.0, 2)[0]
+        gap = 10
+        cv2.rectangle(im0, (20 - gap, 70 - text_size[1] - gap), (20 + text_size[0] + gap, 70 + gap), (255, 255, 255), -1)
+        cv2.putText(im0, text, (20, 70), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 0), 2)
+
     def plot_bboxes(self, results, im0):
         class_ids = []
         self.annotator = Annotator(im0, 3, results[0].names)
