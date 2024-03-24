8637c53b10633faa7ec85e544405e49aa730e7e2
Matthew En <86386184+Mcen25@users.noreply.github.com>
2024-03-23 21:31:26 -0400

    Added email feature

---
+++ email_settings.py
@@ -0,0 +1,3 @@
+password = "jvxj esqv strb spsh"
+from_email = "matthew.en.c@gmail.com"  # must match the email used to generate the password
+to_email = "en.matthew.c@gmail.com"  # receiver emails
--- yolov8.py
+++ yolov8.py
@@ -1,5 +1,10 @@
+import cv2
 from ultralytics import YOLO
+import time
+import os
+
+# Get the path to the Downloads folder
+downloads_folder = os.path.expanduser('~/Downloads')
 
 model = YOLO('yolov8n.pt') # pass any model type
 results = model(source=0,show = True, conf=0.4,save=True)
-#testing to connect to app
